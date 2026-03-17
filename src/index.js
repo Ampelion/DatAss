c2df0789da2344bb71238a3e56eb7797b31726ad
Ampelion <spingus@gmail.com>
2026-03-16 18:21:31 -0700

    hgvjhl;p';,jklhyuitiu

--- src/index.js
+++ src/index.js
@@ -6,9 +6,24 @@ import reportWebVitals from './reportWebVitals';
 
 import { Amplify } from 'aws-amplify';
 import { fetchAuthSession } from 'aws-amplify/auth';
-import awsconfig from './aws-exports';
 
-Amplify.configure(awsconfig);
+
+
+Amplify.configure({
+  API: {
+    GraphQL: {
+      endpoint: 'https://7j77zrcx2bdvfaz5iejdmifvhi.appsync-api.us-west-2.amazonaws.com/graphql',
+      region: 'us-west-2',
+      defaultAuthMode: 'iam'
+    }
+  },
+  Auth: {
+    Cognito: {
+      identityPoolId: 'us-west-2:cdb2926d-aba0-41e4-ae0c-1d02f283b2cd',
+      region: 'us-west-2'
+    }
+  }
+});
 
 fetchAuthSession().then(() => {
   const root = ReactDOM.createRoot(document.getElementById('root'));
