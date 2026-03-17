df4f9f5858dae25bf7ca550f0a90eba4d4523f35
Ampelion <spingus@gmail.com>
2026-03-16 23:15:27 -0700

    one more index.js

--- src/index.js
+++ src/index.js
@@ -4,11 +4,17 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Amplify } from 'aws-amplify';
-import awsconfig from './aws-exports';
 
-
-Amplify.configure(awsconfig);
-
+Amplify.configure({
+  API: {
+    GraphQL: {
+      endpoint: 'https://o3n6ktkvazezbothmhvfdhmjmu.appsync-api.us-west-2.amazonaws.com/graphql',
+      region: 'us-west-2',
+      defaultAuthMode: 'apiKey',
+      apiKey: 'da2-ie4ll3d6bnculhi7p542gjtjke'
+    }
+  }
+});
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
