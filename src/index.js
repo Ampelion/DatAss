1a54cda2a6cd2b04824c06d06ee811abce6020a4
Ampelion <spingus@gmail.com>
2026-03-16 20:56:33 -0700

    5hru44whynextl7plvlqccg5ry

--- src/index.js
+++ src/index.js
@@ -20,7 +20,8 @@ Amplify.configure({
   Auth: {
     Cognito: {
       identityPoolId: 'us-west-2:cdb2926d-aba0-41e4-ae0c-1d02f283b2cd',
-      allowGuestAccess: true
+      allowGuestAccess: true,
+      region: 'us-west-2'
     }
   }
 });
